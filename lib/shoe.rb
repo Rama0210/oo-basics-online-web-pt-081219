0e83f88ab5e8c3962c6f2ae59b4f8c3bfad5711f
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-10 13:51:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,4 +5,9 @@ attr_accessor :brand, :color, :size, :material, :condition
 def initialize(brand)
   @brand = brand
 end
+
+def cobble
+    puts "Your shoe is as good as new!"
+    @condition= "new"
+  end
 end
